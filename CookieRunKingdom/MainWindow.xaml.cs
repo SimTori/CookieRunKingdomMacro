cd688ab02b1c301c219e028224d60fdc699360ce
SimTori <cjh6574@gmail.com>
2022-03-02 23:57:41 +0900

    Update

--- CookieRunKingdom/MainWindow.xaml.cs
+++ CookieRunKingdom/MainWindow.xaml.cs
@@ -32,40 +32,33 @@ namespace CookieRunKingdom
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            List<Process> processesList = Util.GetProcessList("Nox");
-            if (processesList.Count == 0)
-            {
-                MessageBox.Show("실행중인 프로세서가 없습니다.");
-                Close();
-                return;
-            }
-
-            for (int i = 0; i < processesList.Count; i++)
-            {
-                Process process = processesList[i];
-                cbProcessList.Items.Add(process.MainWindowTitle);
-            }
-
-            cbProcessList.SelectedIndex = 0;
+            AddProcess();
         }
 
         private void cbProcessList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ComboBox comboBox = (ComboBox)sender;
+            if (comboBox.Items.Count > 0)
+            {
+                string windowTitle = comboBox.SelectedValue.ToString();
 
-            string windowTitle = comboBox.SelectedValue.ToString();
+                _hwnd = Dll.FindWindow(null, windowTitle);
+                // 녹스의 경우 아래까지 지금은 녹스만 사용
+                _hwnd = Dll.FindWindowEx(_hwnd, 0, "Qt5QWindowIcon", "ScreenBoardClassWindow");
 
-            _hwnd = Dll.FindWindow(null, windowTitle);
-            // 녹스의 경우 아래까지 지금은 녹스만 사용
-            _hwnd = Dll.FindWindowEx(_hwnd, 0, "Qt5QWindowIcon", "ScreenBoardClassWindow");
-
-            //Rectangle rect = Util.GetWindowRect(_hwnd);
-            _winRect = Util.GetWindowRect(_hwnd);
+                //Rectangle rect = Util.GetWindowRect(_hwnd);
+                _winRect = Util.GetWindowRect(_hwnd);
+            }
         }
 
         private void btnCapture_Click(object sender, RoutedEventArgs e)
         {
             capture();
+        }
+
+        private void btnPrpcessRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            AddProcess();
         }
     }
 }
---
+++ CookieRunKingdom/Partial/Function.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CookieRunKingdom
+{
+    public partial class MainWindow
+    {
+        private void AddProcess()
+        {
+            cbProcessList.Items.Clear();
+            List<Process> processesList = Util.GetProcessList("Nox");
+            if (processesList.Count == 0)
+            {
+                MessageBox.Show("실행중인 프로세서가 없습니다.");
+                Close();
+                return;
+            }
+
+            for (int i = 0; i < processesList.Count; i++)
+            {
+                Process process = processesList[i];
+                cbProcessList.Items.Add(process.MainWindowTitle);
+            }
+
+            cbProcessList.SelectedIndex = 0;
+        }
+    }
+}
